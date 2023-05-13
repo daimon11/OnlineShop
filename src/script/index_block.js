a135700d758b0019f7b4a6db3a15a19fd85e867c
Dmitriy Bezgodko <daimon.94@mail.ru>
2023-05-13 23:26:12 +0300

    first

---
+++ src/script/index_block.js
@@ -0,0 +1,42 @@
+import { paginationControl } from './modules/pagination.js';
+import { renderGoods } from './modules/renderArticleList.js';
+import { renderArticle } from './modules/renderArticlePage.js'
+
+const renderArticles = (page) => {
+
+  const btnPrev = document.querySelector('.pagination__btn-prev');
+  const btnNext = document.querySelector('.pagination__btn-next');
+  const pagesBtn = document.querySelectorAll('.item');
+  console.log(pagesBtn);
+
+  paginationControl(btnPrev, btnNext, pagesBtn, page);
+  renderGoods(page);
+}
+
+let url = window.location.search;
+let searchParams = new URLSearchParams(url);
+if (searchParams.has("id")) {
+  let id = searchParams.get("id") ?
+    searchParams.get("id") : '';
+
+  const articleModal = document.querySelector('.article__wrapper');
+
+  renderArticle(    
+    id,
+    articleModal
+    );
+} else {
+  let page = searchParams.get("page") ?
+    searchParams.get("page") : 1;
+
+  let currentPage = page;
+
+  let currentActive = +currentPage;
+
+  renderArticles(currentActive);
+};
+
+
+
+
+
