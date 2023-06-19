1f5801c3d7da7a571af1b395c7185f37b695b0d4
Dmitriy Bezgodko <daimon.94@mail.ru>
2023-06-19 23:53:37 +0300

    add critical

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,18 @@ import htmlMin from 'gulp-htmlmin';
 import cleanCss from 'gulp-clean-css';
 import terser from 'gulp-terser';
 import sourceMaps from 'gulp-sourcemaps';
+import gulpImg from 'gulp-image';
+import gulpWebp from 'gulp-webp';
+import gulpAvif from 'gulp-avif';
+import { stream as critical } from 'critical';
+import gulpif from 'gulp-if';
 // задачи
 
 const prepros = true;
 
-const sass = gulpSass(sassPkg);
+let dev = true;
 
-// const allJs = [
-//   'src/libs/'
-// ]
+const sass = gulpSass(sassPkg);
 
 export const html = () => gulp
   .src('src/*.html')
@@ -34,20 +37,20 @@ export const style = () => {
   if (prepros) {
     return gulp
       .src('src/scss/**/*.scss')
-      .pipe(sourceMaps.init())
+      .pipe(gulpif(!dev, sourceMaps.init()))
       .pipe(sass().on('error', sass.logError))
       .pipe(cleanCss({
         2: {
           specialComments: 0,
         }
       }))
-      .pipe(sourceMaps.write('../maps'))
+      .pipe(gulpif(dev, sourceMaps.write('../maps')))
       .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
   }
   return gulp
     .src('src/style/index.css')
-    .pipe(sourceMaps.init())
+    .pipe(gulpif(dev, sourceMaps.init()))
     .pipe(cssImport({
       extensions: ['css'],
     }))
@@ -56,24 +59,66 @@ export const style = () => {
         specialComments: 0,
       }
     }))
-    .pipe(sourceMaps.write('../maps'))
+    .pipe(gulpif(dev, sourceMaps.write('../maps')))
     .pipe(gulp.dest('dist/style'))
     .pipe(browserSync.stream());
 }
 
 export const js = () => gulp
   .src('src/script/**/*.js')
-  .pipe(sourceMaps.init())
+  .pipe(gulpif(dev, sourceMaps.init()))
   .pipe(terser())
-  .pipe(sourceMaps.write('../maps'))
+  .pipe(gulpif(dev, sourceMaps.write('../maps')))
   .pipe(gulp.dest('dist/js'))
   .pipe(browserSync.stream());
 
+export const img = () => gulp
+  .src('src/img/**/*.{jpeg,jpg,png,svg,gif,jfif}')
+  .pipe(gulpif(!dev, gulpImg({
+    optipng: ['-i 1', '-strip all', '-fix', '-o7', '-force'],
+    pngquant: ['--speed=1', '--force', 256],
+    zopflipng: ['-y', '--lossy_8bit', '--lossy_transparent'],
+    jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
+    mozjpeg: ['-optimize', '-progressive'],
+    gifsicle: ['--optimize'],
+    svgo: true,
+  })))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
+export const webp = () => gulp
+  .src('src/img/**/*.{jpeg,jpg,png,jfif}')
+  .pipe(gulpWebp({
+    quality: 60
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
+export const avif = () => gulp
+  .src('src/img/**/*.{jpeg,jpg,png,jfif}')
+  .pipe(gulpAvif({
+    quality: 60
+  }))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(browserSync.stream());
+
+export const critCSS = () => gulp
+  .src('dist/*.html')
+  .pipe(
+    critical({
+      base: 'dist/',
+      inline: true,
+      css: ['dist/css/index.css'],
+    })
+    // console.log('все должно работать');
+  )
+  .on('error', err => {
+    console.error(err.message)
+  })
+  .pipe(gulp.dest('dist'))
+
 export const copy = () => gulp
-  .src([
-    'src/fonts/**/*',
-    'src/img/*'
-  ], {
+  .src('src/fonts/**/*', {
     base: 'src'
   })
   .pipe(gulp.dest('dist'))
@@ -93,18 +138,22 @@ export const server = () => {
 
   gulp.watch('./src/**/*.html', html);
   gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
+  gulp.watch('src/img/**/*.{jpeg,jpg,png,svg,gif,jfif}', img);
   gulp.watch('./src/**/*.html', js);
-  gulp.watch([
-    './src/style/**/img/*',
-    './src/fonts/**/*'
-  ], copy);
+  gulp.watch('./src/fonts/**/*', copy);
+  gulp.watch('src/img/**/*.{jpeg,jpg,png,jfif}', avif);
+  gulp.watch('src/img/**/*.{jpeg,jpg,png,jfif}', webp);
 }
 
 export const clear = () => deleteAsync('dist/**/*', { forse: true, });
 
 // запуск
-export const base = gulp.parallel(html, style, js, copy);
+export const develop = async () => {
+  dev = true;
+}
 
-export const build = gulp.series(clear, base);
+export const base = gulp.parallel(html, style, js, img, avif, webp, copy);
 
-export default gulp.series(base, server);
+export const build = gulp.series(clear, critCSS, base);
+
+export default gulp.series(develop, base, server);
