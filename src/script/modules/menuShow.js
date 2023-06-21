effad87dff6055e7d4a7b97190feac2fd6b1a947
Dmitriy Bezgodko <daimon.94@mail.ru>
2023-06-21 22:33:50 +0300

    add dist

--- src/script/modules/menuShow.js
+++
@@ -1,8 +0,0 @@
-export const menuControl = (btn, menu) => {
-  btn.addEventListener('click', () => {
-    document.body.classList.toggle('disable-scroll');
-    btn.classList.toggle('header__btn-menu--type_close');
-    btn.classList.toggle('header__btn-menu--type_open');
-    menu.classList.toggle('header__menu-group--type_close');
-  })
-}
